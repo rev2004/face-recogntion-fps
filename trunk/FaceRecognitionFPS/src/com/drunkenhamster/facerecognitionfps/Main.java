d58d61c03fe6ef0ab9e6815455cfaf5f47e0133f
deluxz <deluxz@63a3cd62-1355-9779-4473-e7f34e1b3507>
2010-12-10 15:34:55 +0000

    FaceRecognitionFPS Applicatie set-up

---
+++ trunk/FaceRecognitionFPS/src/com/drunkenhamster/facerecognitionfps/Main.java
@@ -0,0 +1,21 @@
+package com.drunkenhamster.facerecognitionfps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Main Menu Activity
+ * @author Harrie Essing - deluxz@gmail.com
+ * 
+ * Main menu voor het spel.
+ * 
+ */
+
+public class Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
